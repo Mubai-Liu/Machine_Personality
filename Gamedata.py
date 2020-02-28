48ece9b1af1e5a920c20c651ad85616c92ac6407
Mubai Liu <liumubai000@126.com>
2020-02-27 22:00:58 -0500

    Update player data

--- Gamedata.py
+++ Gamedata.py
@@ -3,53 +3,38 @@ import urllib.request
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-
+import csv
 from time import sleep
 from random import randint
-
-
-url = 'https://steamcommunity.com/app/570/discussions/'
-
- 
-
-Link = []
-Id = []
-
-
-ourUrl=urllib.request.urlopen(url).read().decode("utf-8")
-    
-soup=BeautifulSoup(ourUrl,'html.parser')
-    #print(soup.prettify())
-    
-    #all_info=soup.find_all('div',{'class':"responsive_page_content"})
-    #print(all_info)
-    
-    #for i in range(1,len(all_info)):
-for item in  soup.find_all('a',{'class':'forum_topic_overlay'}):
-    link = item.get('href')
-        #print(href)
-    Link.append(link)
-Link
-
-for i in range(len(Link)):
-    Id.append(Link[i].rsplit('/')[-2])
-Id
-
-
-matches = pd.DataFrame()
-history = pd.DataFrame()
-match_id = [5264233901,5263347490]
-for i in match_id:
-    print(i)
-    matches = matches.append(get_match(i))
+# csv file name 
+filename = "Dota_buff.csv"
 api = dota2api.Initialise("54699EC1DAEC662D994DAADE57148354", raw_mode = True)
-pd.DataFrame(api.get_match_details(match_id)['players'])
+rows = [] 
+  
+# reading csv file 
+with open(filename, 'r') as csvfile: 
+    # creating a csv reader object 
+    csvreader = csv.reader(csvfile) 
+      
+    # extracting field names through first row 
+    #fields = csvreader.next() 
+  
+    # extracting each data row one by one 
+    for row in csvreader: 
+        rows.append(row) 
+  
+    # get total number of rows 
+    print("Total no. of rows: %d"%(csvreader.line_num)) 
+  
 
 def get_match(match_id):
-    return pd.DataFrame(api.get_match_details(match_id))
+    match = pd.DataFrame()
+    try:
+        match = api.get_match_details(match_id)
+    except:
+        pass
+    return match
 
-
-# 76561198140336085 - 76561197960265728
 
 def get_history(id):
     match_hist = pd.DataFrame()
@@ -59,30 +44,43 @@ def get_history(id):
         pass
     return match_hist
 
+url = 'https://steamcommunity.com/app/570/discussions/'
+
+matches = pd.DataFrame()
+history = pd.DataFrame()
+
+
+Id = pd.DataFrame(rows)[1]
+Id = Id.to_list()
+Id = Id[1:]
 for item in Id:
     history = history.append(pd.DataFrame(get_history(item)))
-get_history(Id[1])
-pd.DataFrame(api.get_match_history(76561198140336085))
 
 
-r = requests.get("https://api.opendota.com/api/players/76561198140336085/recentMatches")
+# Player history file
+history.to_csv("player_history.csv")
+match_id =[x.get('match_id',0) for x in history['matches']]
+
+for i in match_id:
+    matches = matches.append(get_match(i), ignore_index = True)
+
+
+matches.to_csv("match_details.csv")
+
+player_details = pd.DataFrame()
+for i in range(10):
+    player_details = player_details.append(pd.DataFrame(matches.players.to_list())[i].apply(pd.Series))
+
+
+player_details.to_csv("player_match_details.csv")
+
+# get_history(Id[1])
+# pd.DataFrame(api.get_match_history(76561198140336085))
 
 
 
-pprint.pprint(json.loads(r.text))
 
+# !pip install -U dota2
+# import dota2
+# dota.request_match_details(match_id)
 
-
-
-url = "https://api.opendota.com/api/matches/" + str(match_id)
-requests.get(url)
-
-json.loads(requests.get(url).text)
-
-
-!pip install -U dota2
-import dota2
-dota.request_match_details(match_id)
-
-
-class(matches)
